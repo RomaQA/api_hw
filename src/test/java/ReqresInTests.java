56af9061307d8b33d11ba834e06f624437c4cd39
Roman Vasin <rvasin@dev.vtb.ru>
2023-01-24 23:38:50 +0300

    первые два теста

---
+++ src/test/java/ReqresInTests.java
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.is;
+
+public class ReqresInTests {
+
+    @Test
+    public void createUser(){
+        String data = "{\n" +
+                "    \"name\": \"morpheus\",\n" +
+                "    \"job\": \"leader\"\n" +
+                "}";
+
+        given()
+                .log().uri()
+                .contentType(JSON)
+                .body(data)
+                .when()
+                .post("https://reqres.in/api/users")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(201)
+                .body("name", is("morpheus"));
+    }
+
+    @Test
+    public void singleUserNotFound(){
+        given()
+                .log().uri()
+                .when()
+                .get("https://reqres.in/api/users23")
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(404);
+    }
+    }
+
+
